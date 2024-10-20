b7a1ca65fa1b44ad719b4e5824a77231bfb78485
Ryan Menezes <ryanoliveiram2015@gmail.com>
2024-10-20 17:48:38 -0300

    Fix: Adicionando porta

--- backend/Api/Configs/Server/server.ts
+++ backend/Api/Configs/Server/server.ts
@@ -1,6 +1,6 @@
 import Express from 'express';
 // import routes from '../../Routers/router';
-const port = process.env.PORT || "3005";
+const port = process.env.PORT || 3005;
 const Server = Express();
 
 //Server.use(Express.urlencoded({ extended: true }));
--- backend/build/Configs/Server/server.js
+++ backend/build/Configs/Server/server.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Server = exports.port = void 0;
 const express_1 = __importDefault(require("express"));
 // import routes from '../../Routers/router';
-const port = process.env.PORT || "3005";
+const port = process.env.PORT || 3005;
 exports.port = port;
 const Server = (0, express_1.default)();
 exports.Server = Server;
