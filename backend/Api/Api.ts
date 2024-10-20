56eb89e20f6818163d62f79f57f113c7df7daf48
Ryan Menezes <ryanoliveiram2015@gmail.com>
2024-10-20 17:59:51 -0300

    Fix: devs

--- backend/Api/Api.ts
+++ backend/Api/Api.ts
@@ -2,10 +2,32 @@ import { Server, port } from './Configs/Server/server';
 import { appWhatsapp } from './Configs/whatsConnect/functionsWhatsApp';
 import { ImageResponse } from './Configs/Interfaces/interfaces';
 import { create, CreateOptions } from '@wppconnect-team/wppconnect';
+import puppeteer from 'puppeteer-extra';
+import fs from 'fs';
+import path from 'path';
+
+// Definindo o caminho do Chrome
+const chromePath = '/opt/render/.cache/puppeteer/chrome/linux-130.0.6723.58/chrome-linux64/chrome';
+
+// Função para logar o conteúdo do diretório do Chrome
+function logChromeDirectory(directoryPath: string) {
+    console.log(`Listando arquivos no diretório: ${directoryPath}`);
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.error(`Erro ao listar o diretório ${directoryPath}:`, err);
+            return;
+        }
+        console.log(`Conteúdo do diretório ${directoryPath}:`);
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
+}
+
+// Verificando o diretório onde o Chrome está localizado
+logChromeDirectory(path.dirname(chromePath));
 
 // Definindo as opções de criação
-const puppeteer = require('puppeteer-extra');
-
 const createOptions: CreateOptions = {
     session: 'sessionTeste',
     catchQR: (base64Qr: string, asciiQR: string) => {
@@ -26,13 +48,17 @@ const createOptions: CreateOptions = {
     logQR: true,
     puppeteerOptions: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: '/opt/render/.cache/puppeteer/chrome/linux-130.0.6723.58/chrome-linux64/chrome' // Caminho fixo do Chrome/Chromium
+        args: ['--no-sandbox', '--disable-setuid-sandbox'], // Argumentos para evitar problemas em ambientes restritos
+        executablePath: chromePath // Caminho fixo do Chrome/Chromium
     }
 };
 
 // Verificando se o caminho do Chrome/Chromium está correto
-console.log(`Usando o caminho do executável: ${puppeteer.executablePath()}`);
+if (createOptions.puppeteerOptions) {
+    console.log(`Usando o caminho do executável: ${createOptions.puppeteerOptions.executablePath}`);
+} else {
+    console.error('puppeteerOptions não está definido.');
+}
 
 Server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
